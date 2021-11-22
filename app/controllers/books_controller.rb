0324d2a5de5950f581cef17c1e0679b391c541f9
yamaguchishuhe <shuhei0976820@gmail.com>
2021-11-22 11:38:38 +0000

    gokaime

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,9 @@ class BooksController < ApplicationController
 	def edit
 		@book = Book.find(params[:id])
 		@user = User.find(@book.user_id)
+		if @user != current_user
+		    redirect_to books_path
+		end
 	end
 
 	def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,10 @@ class UsersController < ApplicationController
 
   def edit
    @user = User.find(params[:id])
+  	if @user != current_user
+		    redirect_to user_path(current_user.id)
 
+  	end
   end
   def update
    @user = User.find(params[:id])
